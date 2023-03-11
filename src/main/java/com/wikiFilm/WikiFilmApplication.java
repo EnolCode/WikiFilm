59306f50be2847960c167b56f57b22f4be745052
Enol-Igareta <enol1code@gmail.com>
2023-03-11 10:48:31 +0100

    init repo

---
+++ src/main/java/com/wikiFilm/WikiFilmApplication.java
@@ -0,0 +1,13 @@
+package com.wikiFilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiFilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wikiFilm/WikiFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wikiFilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
