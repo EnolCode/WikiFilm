b79a00d7c5fb019a916f7a1be572aa9ecc3764df
Enol-Igareta <enol1code@gmail.com>
2023-03-14 20:49:56 +0100

    film entity

---
+++ src/main/java/com/wikiFilm/models/Film.java
@@ -0,0 +1,30 @@
+package com.wikiFilm.models;
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "films")
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true)
+    private LocalDate releaseDate;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(nullable = true)
+    private String description;
+}
