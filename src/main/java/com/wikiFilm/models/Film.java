157b95936048b0e8ae885b9231a48f4d07379f69
Enol-Igareta <enol1code@gmail.com>
2023-03-19 22:10:11 +0100

    genre entity

--- src/main/java/com/wikiFilm/models/Film.java
+++ src/main/java/com/wikiFilm/models/Film.java
@@ -15,7 +15,7 @@ public class Film {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 45)
     private String title;
 
     @Column(nullable = true)
---
+++ src/main/java/com/wikiFilm/models/Genre.java
@@ -0,0 +1,27 @@
+package com.wikiFilm.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "genres")
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 45)
+    private String name;
+}
