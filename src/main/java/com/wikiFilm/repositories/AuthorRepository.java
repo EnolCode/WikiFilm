b77b8abfb3fce05611e0491bce2681b49fc207e8
Enol-Igareta <enol1code@gmail.com>
2023-03-21 21:45:01 +0100

    entity author

---
+++ src/main/java/com/wikiFilm/repositories/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.wikiFilm.repositories;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.wikiFilm.models.Author;
+
+public interface AuthorRepository extends JpaRepository<Author, Long>{
+
+    Optional<Author> findByAuthor(String name);
+
+}
