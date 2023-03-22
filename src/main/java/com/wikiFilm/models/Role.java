42c50341f80b084ea3463eaa55f815b67b6f7735
Enol-Igareta <enol1code@gmail.com>
2023-03-22 22:23:34 +0100

    roles

--- src/main/java/com/wikiFilm/models/Role.java
+++ src/main/java/com/wikiFilm/models/Role.java
@@ -1,35 +1,32 @@
 package com.wikiFilm.models;
 
-
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-@Entity(name = "users")
-public class User {
+@Entity( name = "roles")
+public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
-    private String password;
+    private String roleName;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles;
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    Set<User> users;
 }
