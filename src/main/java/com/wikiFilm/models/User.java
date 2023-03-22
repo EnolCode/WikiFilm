7b9b16f054f34d0151375ba65102e9e9dd52e4cf
Enol-Igareta <enol1code@gmail.com>
2023-03-22 22:15:48 +0100

    user

---
+++ src/main/java/com/wikiFilm/models/User.java
@@ -0,0 +1,27 @@
+package com.wikiFilm.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    
+    
+    
+}
