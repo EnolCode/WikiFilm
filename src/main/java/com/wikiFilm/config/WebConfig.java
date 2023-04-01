c28cdfe7f0458a0ae19dd1214526758af6f8f322
Enol-Igareta <enol1code@gmail.com>
2023-04-01 11:24:11 +0200

    cookies

--- src/main/java/com/wikiFilm/config/WebConfig.java
+++ src/main/java/com/wikiFilm/config/WebConfig.java
@@ -15,4 +15,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "DELETE", "OPTION", "PUT");
     }
+
 }
